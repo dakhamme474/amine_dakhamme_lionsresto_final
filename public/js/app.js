9f5a62390fcccaa34994444fe92a39c0899ba8dd
dakhamme474 <dakhamme474@gmail.com>
2023-06-23 15:29:01 +0100

    end tamplate without slide

--- public/js/app.js
+++ public/js/app.js
@@ -2,35 +2,40 @@
 let menu_liste = document.querySelector(".menu-burger")
 let links = document.querySelector(".nav-info")
 
-const menu_burger = () =>{
- links.classList.toggle("open-menu")
+const menu_burger = () => {
+    links.classList.toggle("open-menu")
 }
 
-menu_liste.addEventListener("click" , menu_burger)
+menu_liste.addEventListener("click", menu_burger)
 
 let scroll_up = document.querySelector(".up")
-window.onscroll = function (){ 
-    if (this.scrollY >= 500 ) {
+window.onscroll = function () {
+    if (this.scrollY >= 500) {
         scroll_up.classList.add("show")
-    }else{
+    } else {
         scroll_up.classList.remove("show")
 
     }
 }
-scroll_up.addEventListener("click" , ()=>{
+scroll_up.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth",
-        });
+    });
 })
 
 
-// !let xwatch = document.querySelector(".watch-video")
-// !let place_video = document.querySelector(".vid-vid")
 
-// !const xvideo_add = () =>{
-// !place_video.classList.add(".modal-video-1");
 
-// !};
-// !xwatch.addEventListener("click" , xvideo_add)
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function openPopup() { document.getElementById("popupOverlay").style.display = "flex"; } function closePopup() { document.getElementById("popupOverlay").style.display = "none"; }
+
+
+
+
+
+
+
 
