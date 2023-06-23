20919344feffec6361cd0954d35fd79f09e6e7f2
dakhamme474 <dakhamme474@gmail.com>
2023-06-23 05:36:07 +0100

    add scroll to up

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,23 @@ const menu_burger = () =>{
 
 menu_liste.addEventListener("click" , menu_burger)
 
+let scroll_up = document.querySelector(".up")
+window.onscroll = function (){ 
+    if (this.scrollY >= 500 ) {
+        scroll_up.classList.add("show")
+    }else{
+        scroll_up.classList.remove("show")
+
+    }
+}
+scroll_up.addEventListener("click" , ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+        });
+})
+
+
 // !let xwatch = document.querySelector(".watch-video")
 // !let place_video = document.querySelector(".vid-vid")
 
